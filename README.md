3fdf7d7a77dfc0352f3bea1863a4e2f21ca4a96e
colodoo <lodog1@qq.com>
2017-02-12 11:54:50 +0800

    ColodooPoc tool class

---
+++ README.md
@@ -0,0 +1,2 @@
+# ColodoPoc
+ColodoPoc框架 v1.0
---
+++ tool/poc_tool.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2017/2/12 1:26
+# @Author  : CoLoDoo
+# @Site    : 
+# @File    : poc_tool.py
+# @Software: PyCharm
+
+import requests
+import json
+
+class PocTool:
+    """docstring for PocClass"""
+    timeout = 10
+    headers = (
+        {"User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.9; rv:5.0) Gecko/20100101 Firefox/5.0",
+         "Accept": "text/plain"},
+        {
+            "User-Agent": "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/45.0.2454.101 Safari/537.36"},
+        {
+            "User-Agent": "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36"},
+    )
+    codes = (
+        200, 500, 406, 401
+    )
+
+    # 初始化
+    def __init__(self, url):
+        self.url = url
+
+    # 包含识别
+    # 返回结果为0 or 1
+    def check_key_in_value(self, key, value):
+        if key in value:
+            return 1
+        else:
+            return 0
+
+    # 取得相应请求
+    # 返回结果为请求
+    def get_request(self, url='', headerNum=0):
+        tmp_url = url if url != '' else self.url
+        if headerNum != 0:
+            request = requests.get(tmp_url, headers=self.headers[headerNum], verify=False, timeout=self.timeout)
+        else:
+            request = requests.get(tmp_url, headers=self.headers[0], verify=False, timeout=self.timeout)
+        return request
+
+    # 取得url中的参数
+    # 以列表的形式返回
+    def get_send_values(self):
+        values_list = list()
+        values_string = self.url.split('?')[1:][0]
+
+        for tmp in values_string.split('&'):
+            values_list.append(tmp.split('='))
+
+        return values_list
+
+    # 把list对象转换成json对象
+    def list_dic_tuple_2json(self, list_dic_tuple):
+        return json.dumps(list_dic_tuple, indent=4)
+
+if __name__ == '__main__':
+	tmp = PocTool('http://www.zengyf.com/index_comic_show2.action?id=258&page=1&title=就叫薏米好了')
+	print tmp.get_request(headerNum=2).content
+	# print tmp.list_dic_tuple_2json(tmp.get_send_values())
+	# help(PocTool)
